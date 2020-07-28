bdd7f26fe2936d3cc592cc54a681d80798845077
rootZWH <214704267@qq.com>
2020-07-28 19:14:36 +0800

    28 下午第一节

--- src/components/room/main.vue
+++ src/components/room/main.vue
@@ -16,24 +16,25 @@
            <table class="table table-bordered">
 			  <thead>
 			    <tr>
-			      
 			      <th scope="col">编号</th>
 			      <th scope="col">编码</th>
-			      <th scope="col">名称</th>
+			      <th scope="col">类型</th>
+				  <th scope="col">价格</th>
 			      <th scope="col">操作</th>
 			    </tr>
 			  </thead>
 			  <tbody>
-			  <c:forEach items="${departmentList}" var="dm">
-			    <tr>
-			      <td>${dm.no }</td>
-			      <td>${dm.code }</td>
-			      <td>${dm.name }</td>
-			      <td><a href="tomodify.mvc" class="btn btn-success">修改</a> 
-			      <a href="todelete.mvc" class="btn btn-danger">删除 </a> 
-			      <a href="toview.mvc" class="btn btn-default">查看</a> </td>
+			    <tr v-for="rm in roomList" v-bind:key="rm.no">
+			      <td>{{rm.no}}</td>
+			      <td>{{rm.code}}</td>
+			      <td>{{rm.type}}</td>
+				  <td>{{rm.price}}</td>
+			      <td>
+					  <a href="tomodify.mvc" class="btn btn-success">修改</a> 
+					  <a href="todelete.mvc" class="btn btn-danger">删除 </a> 
+					  <a href="toview.mvc" class="btn btn-default">查看</a> 
+				  </td>
 			    </tr>
-			   </c:forEach>
 			  </tbody>
 			</table>
         </div>
@@ -44,10 +45,35 @@
 </template>
 
 <script>
+	import axios from "axios";
 	export default{
 		name:"RoomMain",
 		data(){
-			return {};
+			return {
+				roomList:[],
+				page:1,
+				rows:10,
+				count:0,
+				pageCount:0
+			};
+		},
+		created(){//组件的生命周期方法 组件创建以后
+			this.getList();
+		},
+		methods:{
+			getList(){
+				
+				axios.get("http://localhost:8100/room/list/all/page",{
+					params:{
+						rows:this.rows,
+						page:this.page
+					}
+				}).then(result=>{
+					this.roomList=result.data.list;
+					this.count=result.data.count;
+					this.pageCount=result.data.pageCount;
+				});
+			}
 		}
 	}
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+
 //引入路由管理的组件
 //首页区组件
+import AdminLogin from "./../components/admin/login.vue"
 import HomeMain from "./../components/home/main.vue"
 import RoomMain from "./../components/room/main.vue"
 import GoodsMain from "./../components/goods/main.vue"
@@ -11,6 +13,7 @@ Vue.use(VueRouter)
 
 const routes = [
 	{path:"/",name:"homemain",component:HomeMain},
+	{path:"/login",name:"login",component:AdminLogin},
 	{path:"/room",name:"roommain",component:RoomMain},
 	{path:"/goods",name:"gooodsmain",component:GoodsMain}
 	
